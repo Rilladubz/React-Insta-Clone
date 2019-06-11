4e95bad6e189d8b62abfcd747f007a381cfaafb7
Samuel Torres <rillatube@gmail.com>
2019-06-10 23:39:11 -0400

    End day commit

---
+++ instagram-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// ________________Component-Imports__________________
+import MainPage from './components/MainPage/MainPage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
